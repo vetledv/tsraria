34da3913927ef0bc5674a933ca5fbb96088c95ea
Vetle de Vries Brandth <vetle@dvries.com>
2024-05-02 19:52:49 +0200

    format

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = document.getElementById("root")
+if (!root) {
+	throw new Error("No root element")
+}
+createRoot(root).render(
+	<React.StrictMode>
+		<p>123</p>
+	</React.StrictMode>,
 )
