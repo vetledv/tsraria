93a5711b6bbec8d21e7f683d354d33b9295c485c
Vetle de Vries Brandth <vetle@dvries.com>
2024-05-02 20:08:18 +0200

    tanstack router

---
+++ src/routes/__root.tsx
@@ -0,0 +1,22 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router"
+
+export const Route = createRootRoute({
+	component: RootRoute,
+})
+
+function RootRoute() {
+	return (
+		<>
+			<div className="p-2 flex gap-2">
+				{/* <Link to="/" className="[&.active]:font-bold">
+					Home
+				</Link>{" "}
+				<Link to="/about" className="[&.active]:font-bold">
+					About
+				</Link> */}
+			</div>
+			<hr />
+			<Outlet />
+		</>
+	)
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react(), tailwindcss(), TanStackRouterVite({ experimental: { enableCodeSplitting: true } })],
 })
